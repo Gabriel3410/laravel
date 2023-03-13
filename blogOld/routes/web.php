2c89dfbe11bf57b0a61646c1dbae5dd24fc55f86
gabriel soares <gabrielsantossoares18@gmail.com>
2023-03-13 09:09:24 -0300

    criação da pasta blogDois
    estou fazendo testes na pasta blog então fiz um backup para superte

---
+++ blogOld/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostagemController;
+
+
+
+Route::get('/', [PostagemController::class, 'index'])
+->name('/postagem');
+
+Route::get('postagem/{id}/edit', [PostagemController::class, 'edit'])
+->name('postagem.edit');
+
+Route::put('postagem/{id}', [PostagemController::class, 'update'])
+->name('postagem.update');
+ 
+Route::get('postagem/create', [PostagemController::class, 'create'])
+->name('postagem.create');
+
+Route::post('postagem/create', [PostagemController::class, 'store'])
+->name('postagem.store');
+
+Route::delete('postagem/{id}', [PostagemController::class, 'destroy'])
+->name('postagem.destroy');
+
+Route::get('postagem/{id}', [PostagemController::class, 'show'])
+->name('postagem.show'); 
+
