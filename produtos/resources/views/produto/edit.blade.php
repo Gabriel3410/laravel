5518ec7e101faab3b55213c30d01007e24243166
Gabrielsantos3410 <gabrielsantossoares18@gmail.com>
2023-03-16 20:20:42 -0300

    update

--- produtos/resources/views/produto/edit.blade.php
+++ produtos/resources/views/produto/edit.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
     <title>Edição do produto: {{$produto->nome}}</title>
 </head>
-<body>
-
-    <h1>Alterar Produto: {{$produto->nome}}</h1>
-    <div class="row mb-3">
+<body>    
+        <h1 class="centered">Alterar Produto: {{$produto->nome}}</h1>
         <div class="form-group">
             {!! Form::open(['url' => 'produto/'. $produto->id, 'method'=>'put'])!!}
 
@@ -27,7 +31,7 @@
 
             {!! Form::submit('enviar')!!}<br>
             {!! Form::close()!!} 
+            <a class="btn btn-success mt-2" href="{{ route('produto.index') }}">Voltar</a>
         </div>
-    </div>
 </body>
 </html>
